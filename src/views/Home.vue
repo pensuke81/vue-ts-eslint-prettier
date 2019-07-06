a4775febfbe7648451443f4ff8973b9a19c8e9f6
pensuke <pensuke81@gmail.com>
2019-07-06 19:04:21 +0900

    Execute lint --fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,11 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "home",
+  name: 'home',
   components: {
     HelloWorld
   }
